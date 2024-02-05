3ca55eb3a93cf90649b87fa28520486336df9758
Sukhvir Singh <129220391+Sukhvir-Grewal@users.noreply.github.com>
2024-02-05 03:27:16 -0500

    Final Version-34

--- pages/Quiz.js
+++ pages/Quiz.js
@@ -26,7 +26,9 @@ export default function Quiz() {
     const currentQuestionRef = useRef(null);
     let timeoutRef = useRef(null);
     let runningTimeoutRef = useRef(null);
-    const optionContainerRef = useRef([]);
+    const optionContainerRef = Array.from({ length: 4 }, () => useRef());
+
+    
 
     const [confirmDisplay, setConfirmDisplay] = useState(false);
     const [currentOptionSet, setCurrentOptionSet] = useState(0);
@@ -54,12 +56,6 @@ export default function Quiz() {
 
     const [resultDataType, setResultDataType] = useState(null);
     const [user, setUser] = useAtom(userData);
-
-    useEffect(() => {
-        optionContainerRef.current = Array.from({ length: 4 }).map(() =>
-            createRef(null)
-        );
-    }, []);
 
     useEffect(() => {
         if (!isSignDiv && currentQuestionRef.current) {
