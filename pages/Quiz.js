0698e966b9d02cad46de862573919a724cffe225
Sukhvir Singh <129220391+Sukhvir-Grewal@users.noreply.github.com>
2024-02-05 03:07:10 -0500

    Final Version-31

--- pages/Quiz.js
+++ pages/Quiz.js
@@ -26,7 +26,7 @@ export default function Quiz() {
     const currentQuestionRef = useRef(null);
     let timeoutRef = useRef(null);
     let runningTimeoutRef = useRef(null);
-    const optionContainerRef = Array.from({ length: 4 }).map(() => useRef(null));
+    const optionContainerRef = Array.from({ length: 4 }).map(() => createRef(null));
 
     
 
