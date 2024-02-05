aaebb500a6f2b8193a934c5a06e3b0c46399415d
Sukhvir Singh <129220391+Sukhvir-Grewal@users.noreply.github.com>
2024-02-05 03:38:06 -0500

    Final Version-37

--- pages/Quiz.js
+++ pages/Quiz.js
@@ -26,9 +26,7 @@ export default function Quiz() {
     const currentQuestionRef = useRef(null);
     let timeoutRef = useRef(null);
     let runningTimeoutRef = useRef(null);
-    const optionContainerRef = Array.from({ length: 4 }, () => useRef());
-
-    
+    const optionContainerRef = Array.from({ length: 4 }, () => createRef());
 
     const [confirmDisplay, setConfirmDisplay] = useState(false);
     const [currentOptionSet, setCurrentOptionSet] = useState(0);
